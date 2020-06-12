a1afd407c046f916e9739c4c9c7934d9c6bd675b
Thomas Elliott <telliott999@gmail.com>
2020-06-12 12:13:58 -0400

    no message

---
+++ us_by_counties.py
@@ -0,0 +1,30 @@
+import sys
+from operator import itemgetter
+
+import myutil.udates as udates
+import myutil.udb as udb
+import myutil.ufmt as ufmt
+import myutil.uinit as uinit
+import myutil.ukeys as ukeys
+import myutil.umath as umath
+import myutil.ustrings as ustrings
+
+
+conf = uinit.clargs()
+mode = conf['mode']
+
+date_info, D = udb.load_db()
+conf['first'], conf['last'] = date_info.split('\n')
+
+#-------------------------
+
+kL = ukeys.key_list_for_us_counties()
+rL = [D[k][mode] for k in kL]
+
+labels = []
+for k in kL:
+    county = ukeys.county_for_key(k)
+    state = ukeys.state_for_key(k)
+    labels.append(county + ', ' + state)
+
+print ufmt.fmt_screen(rL,labels,conf)
